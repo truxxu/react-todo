be02f7ea0645bce398074926ba8d719865dda69c
Daniel Trujillo <danielfetrujillo@gmail.com>
2021-06-15 08:54:52 -0500

    initial setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# TODO App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+In order to run this project, execute the following commands:
+
+* npm install
+* npm start
---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Navbar, List} from '../organisms'
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar />
+            <List />
+        </div>
+    )
+}
+
+export {Main}
