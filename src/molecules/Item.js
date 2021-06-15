f4d047988cc1ba46c7fce7fbc59b0fa10cbf7738
Daniel Trujillo <danielfetrujillo@gmail.com>
2021-06-15 12:17:56 -0500

    added date parsing

--- src/molecules/Item.js
+++ src/molecules/Item.js
@@ -1,16 +1,25 @@
 import React from "react";
+import moment from "moment";
 
 import "./Item.css";
 import { CheckBox } from "../atoms";
 
 const Item = ({ isComplete, description, dueDate }) => {
+  const parseDate = (date) => {
+    if (date) {
+      return <p className="item_date">{moment(date).format("MM/DD/YYYY")}</p>;
+    }
+
+    return;
+  };
+
   return (
     <div className="item_container">
       <div className="item_info">
         <CheckBox checked={isComplete} />
         <p className="item_infoTitle">{description}</p>
       </div>
-      <p>{dueDate}</p>
+      {parseDate(dueDate)}
     </div>
   );
 };
